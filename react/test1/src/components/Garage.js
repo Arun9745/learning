6befee901758e501603b0dab020f336f697b724c
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2023-11-22 16:27:26 +0530

    d

--- react/test1/src/components/Garage.js
+++ react/test1/src/components/Garage.js
@@ -5,11 +5,15 @@ import Car from './Car'
 function Garage() {
     const carinfo={color: "red",brand:"ford"};
     const appleinfo={color: "green",type:"fruit"};
+    const dooropened=false;
+    //const carinfo={}
     return(
         <>
         <h1>Who lives inside my Garage </h1>
-        <Car carinfo={carinfo} />
+        {carinfo.brand !== undefined && carinfo.brand !==undefined ?
+        <Car carinfo={carinfo} /> : null}
         <Apple appleinfo={appleinfo} />
+        {dooropened?<h2> The door is opened</h2>: <h2> The door is not opened</h2>}
         </>
     );
 }
