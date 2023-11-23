3a039d87371ab549bbcdb3d6c22a05259df84479
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2023-11-23 11:55:07 +0530

    w

--- react/test1/src/components/Garage.js
+++ react/test1/src/components/Garage.js
@@ -6,6 +6,12 @@ function Garage() {
     const carinfo={color: "red",brand:"ford"};
     const appleinfo={color: "green",type:"fruit"};
     const dooropened=false;
+    const carlist=[
+        {brand: "BMW" ,color: "Red"},
+        {brand: "Ford" ,color: "White"},
+        {brand: "Tesla" ,color: "blue"},
+    ]
+    const numberlist=[1,2,3,4,5,6];
     //const carinfo={}
     return(
         <>
@@ -14,6 +20,11 @@ function Garage() {
         <Car carinfo={carinfo} /> : null}
         <Apple appleinfo={appleinfo} />
         {dooropened?<h2> The door is opened</h2>: <h2> The door is not opened</h2>}
+        <ul>
+            { carlist.map((carinfo) =><li><Car carinfo={carinfo}/>  </li>)}
+                </ul>
+                <ul>{
+                numberlist.map((e,index)=> <p key={index}>{e}</p>)}</ul>
         </>
     );
 }
