d506bb5c87958e72b1404e4388c7b1e007670011
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2023-11-24 14:42:20 +0530

    w

--- react/react router/src/App.js
+++ react/react router/src/App.js
@@ -5,6 +5,10 @@ import Contact from './components/Contact';
 import Home from './components/Home';
 import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 import User from './components/User';
+import Login from './components/Login';
+import Oldbooks from './components/Oldbooks';
+import Newbooks from './components/Newbooks';
+import Dashboard from './components/Dashboard';
 function App() {
     return (
      <div className="App">
@@ -18,12 +22,20 @@ function App() {
         <li><Link to='/user/2'>user2</Link></li>
         <li><Link to='/books/oldbooks'>Oldbook</Link></li>
         <li><Link to='/books/newbooks'>Newbook</Link></li>
+        <li><Link to='/login'>login</Link></li>
+        
       </ul>
        <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact-us' element={<Contact />} />
         <Route path='/User/:id' element={<User />} />
+        <Route path='/books'>
+          <Route path='oldbooks' element={<Oldbooks />} />
+          <Route path='newbooks' element={<Newbooks />} />
+        </Route>
+        <Route path='Login' element={<Login />} />
+        <Route path='dashboard' element={<Dashboard />} />
        </Routes>
         </BrowserRouter>
      </div>
