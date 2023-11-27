5a48537b470ff8eadbca77c99dcb5f55c66bb864
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2023-11-27 12:08:06 +0530

    d

--- react/react hooks/src/App.js
+++ react/react hooks/src/App.js
@@ -3,14 +3,14 @@ import React ,{useState} from 'react'
 const App = () => {
   const [counter, SetCounter]=useState(0);
 
-  function IncreaseCounter() {
-    SetCounter(counter+1);
+  function DecreaseCounter() {
+    SetCounter(counter-1);
   }
  
   return (
     <div>
       <h1>Counter: {counter}</h1>
-        <button onClick={IncreaseCounter}>Increase</button>
+        <button onClick={DecreaseCounter}>Decrease</button>
       
     </div>
   )
