cc2b2380178463a1c4d1a97da1ceb9c0bd2a0d58
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2023-12-04 15:19:24 +0530

    5

--- react/react hooks/src/components/Counter.js
+++ react/react hooks/src/components/Counter.js
@@ -2,10 +2,10 @@ import React,{useState,useMemo} from "react";
  
 
 const Counter=() =>{
-    const[number,SetNumber]=useState(1);
+    const[number,setNumber]=useState(1);
 
     const factorialOf = (n) => {
-        console.log('inside factorialOf method');
+        console.log('inside factorialOf method ' + n);
         let result=1;
         if(n <=1){
             return result;
@@ -13,18 +13,22 @@ const Counter=() =>{
             for(let i=n;i >=1;i--){
                 result=result * i;
             }
+            console.log(result)
             return result;
         }
     }
 
-   // const factorial=factorialOf(number)
-      const factorial=useMemo(() =>{factorialOf(number)},[number]);
+   //const factorial=factorialOf(number);
+      const factorial= useMemo(() =>{
+        console.log("inside use memo");
+       return  factorialOf(number)
+    },[number]);
 
 
     return(
         <div>
             <p>Find the factorial of the given number</p>
-            <input type="text" placeholder="Enter the number" onChange={(e) => {SetNumber(e.targett.value)}} />
+            <input type="text" placeholder="Enter the number" onChange={(e) => {setNumber(e.target.value)}} />
             <br />
             <span>Factrial of the given number is {factorial} </span>        
             </div>
