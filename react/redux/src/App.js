7d8d089131f11a45cb2f558de0be5c6a663bf96c
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2023-12-05 13:24:56 +0530

    e

---
+++ react/redux/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Customer from './components/Customer';
+
+function App() {
+  return(
+    <div className="App">
+      <h1>React Redux Customer Example</h1>
+      <Customer />
+    </div>
+  );
+} 
+
+export default App;
