9389f4b17b0181de25c4e73231dc7c48f54d9342
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2024-01-08 12:51:45 +0530

    website 1

---
+++ javascript Leetcode Problem/switchcase.js
@@ -0,0 +1,12 @@
+let num=-9;
+switch (num){
+      case num:
+      console.log("positive");
+      break;
+      case num:
+      console.log("negative");
+      break;
+      case num:
+      console.log("null");
+      break;
+}
---
+++ json parse/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Garage from './components/Garage';
+
+function App() {
+    return (
+      <>
+      <h2>Hi I am a Car</h2>
+      <Garage />
+      </>
+    )
+  }
+                        
+export default App;
