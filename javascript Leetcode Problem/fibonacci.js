3f79a0c5ebbbd8561741aee22385628d6573022a
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2024-01-31 17:01:29 +0530

    s

--- javascript Leetcode Problem/fibonacci.js
+++ javascript Leetcode Problem/fibonacci.js
@@ -1,9 +1,15 @@
-const number=parseInt(prompt('Enter the number of terms: '));
+let j=10;
 let n1=0,n2=1,n;
 
-for(let i=1;i<=n;i++){
-    console.log("Fibonacci Series")
+for(let i=1;i<=j;i++){
+    
     n=n1+n2;
     n1=n2;
     n2=n;
+    console.log(n)
 }
+
+
+
+
+//1 2 3 5 8 13 21 
